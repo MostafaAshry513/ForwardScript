58d951529cd09ecc6b4532c0d7ba959f36d8965b
Mostafa Ashry <164094512+MostafaAshry513@users.noreply.github.com>
2024-03-25 20:26:40 +0200

    Add files via upload

---
+++ config.py
@@ -0,0 +1,22 @@
+import os
+
+#The Chat ID You Want To Forward From
+FROM_ID = int(os.getenv("FROM_ID", "The Chat ID You Want To Forward From"))
+
+#The Chat ID You Want To Forward To
+TO_ID = int(os.getenv("TO_ID", "The Chat ID You Want To Forward To"))
+
+#Session String is a Quick Way To Login To Your Account From TG API
+SESSION = os.getenv("SESSION", "Your Session String")
+
+#Revers The Messages
+REVERSED = os.getenv("REVERSED", "True").lower() == "true"
+
+#The Messages Limit You Want To Forward, 0 To Forwrad All The Chat Messages
+LIMIT = int(os.getenv("LIMIT", "Limit Yiu Want"))
+
+# You Can Get API ID And API HASH From: https://core.telegram.org/api/obtaining_api_id
+
+API_ID = int(os.getenv("API_ID", "Your API ID")) #17058698
+
+API_HASH = os.getenv("API_HASH", "Your API Hash") #088f8d5bf0b4b5c0536b039bb6bdf1d2
---
+++ main.py
@@ -0,0 +1,47 @@
+from pyrogram import Client
+from config import *
+
+app = Client(
+	#name=SESSION,
+	session_string=SESSION, 
+	api_id=API_ID, 
+	api_hash=API_HASH,
+	device_model="ForwardScript",
+	in_memory=True
+	)
+
+app.start()
+
+if LIMIT == 0:LIMIT = None
+
+messages = app.get_chat_history(
+	FROM_ID, limit=LIMIT)
+
+msg_list = [msg for msg in messages]
+if REVERSED:
+	msg_list = list(reversed(msg_list))
+
+for message in msg_list:
+	if hasattr(message, 'media') and message.media is not None:
+		media_type = message.media.value
+		media = getattr(message,media_type)
+		if media_type=="web_page":continue
+		file = app.download_media(message)
+		media_handlers = {
+		    "photo": app.send_photo,
+		    "video": app.send_video
+		}
+		handler = media_handlers.get(media_type)
+		if handler:
+			handler(chat_id=TO_ID, **{media_type: file}, caption=message.caption)
+	else:
+		continue
+		if message.text is not None:
+			app.send_message(
+				TO_ID,
+				message.text
+				)
+		else:
+			pass
+
+app.stop()
